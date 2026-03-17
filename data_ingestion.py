41cf86c94738bad9a65abf6f74c71b8472b041ed
sohbatSandhu <sohbatsandhu14@gmail.com>
2026-03-17 12:31:37 -0700

    implement micro-telemetry dataset building

--- data_ingestion.py
+++ data_ingestion.py
@@ -61,7 +61,7 @@ def get_race_session(year_, circuit_name_):
     """
     # get session data
     sessions = request_openf1_data(
-        "sessions", year=year_,circuit_short_name=circuit_name_
+        "sessions", year=year_, circuit_short_name=circuit_name_
     )
 
     # Identify race
@@ -92,16 +92,38 @@ def fetch_driver_telemetry(session_key_, driver_number_):
 
     return df
 
-def download_and_process_telemetry(session_key, drivers, laps):
+def download_and_process_telemetry(session_key, driver_numbers, laps):
+    """
+    Download car telemetery data in batches (per driver) and process into the 
+    required format
 
-    driver_numbers = drivers["driver_number"]
+    Args:
+        session_key (int): Grand Prix Session key
+        driver_numbers (list): List of drivers participating in the session
+        laps (pd.DataFrame): Lap data
+
+    Returns:
+        pd.DataFrame: Processed Car Telemetry data
+    """
+    
+    # columns for telemetry
+    columns = [
+        "date", "driver_number", "lap_number", "micro_sector", 
+        "speed", "throttle", "TimeSeconds", "LapTimeSeconds"
+    ]
+    
+    # ensure lap has start time and end time 
+    laps["date_start"] = pd.to_datetime(laps["date_start"], format='ISO8601')
+    laps["date_end"] = (
+        laps["date_start"] + pd.to_timedelta(laps["lap_duration"], unit="s")
+    )
 
     all_telemetry = []
 
     for driver in driver_numbers:
-
         print(f"Downloading telemetry for driver {driver}")
 
+        # fetch driver telemetry
         df = fetch_driver_telemetry(
             session_key,
             driver
@@ -112,11 +134,14 @@ def download_and_process_telemetry(session_key, drivers, laps):
             continue
         
         print("Assigning Laps to Telemetry...")
-        df = assign_laps_to_telemetry(df, laps        )
+        df = assign_laps_to_telemetry(df, laps)
 
-        print("Building micro-sectors...")
+        print("Building micro-sectors and aggregate...")
         df = build_microsectors(df)
-        all_telemetry.append(df)
+        df = aggregate_microsectors(df)
+        
+        # append data to all telemetry dataframe
+        all_telemetry.append(df[columns])
 
     telemetry = pd.concat(
         all_telemetry,
@@ -124,6 +149,23 @@ def download_and_process_telemetry(session_key, drivers, laps):
     )
 
     return telemetry
+
+def aggregate_microsectors(df):
+    #todo: improve aggregation estimates
+    grouped = df.groupby(
+        ["driver_number", "lap_number", "micro_sector"],
+        as_index=False
+    ).agg({
+        "date" : "min",
+        "TimeSeconds": "sum",
+        "LapTimeSeconds": "mean",
+        # "distance_ratio": "sum",
+        # "cum_distance": "max",
+        "speed": "mean",
+        "throttle": "mean",
+    })
+
+    return grouped.reset_index().drop(columns=["index"])
 
 def build_microsectors(telemetry):
     """
@@ -134,8 +176,20 @@ def build_microsectors(telemetry):
         telemetry (pd.DataFrame): Telemetry data
 
     Returns:
-        _type_: _description_
+        pd.DataFrame: Micro sector telemetry data
     """
+    def update_first_element(group):
+        """
+        Update first element of difference in time seconds.
+        """
+        # Sum all elements in 'TimeSeconds' except for the first one (index 0)
+        remaining_sum = group['TimeSeconds'].iloc[1:].sum()
+        
+        # Calculate the new value for the first element
+        # New Value = lap_duration - sum(remaining elements)
+        group.iloc[0, group.columns.get_loc('TimeSeconds')] = group['LapTimeSeconds'].iloc[0] - remaining_sum
+        return group
+    
     telemetry = telemetry.copy()
 
     # Ensure proper ordering
@@ -145,11 +199,15 @@ def build_microsectors(telemetry):
     )
 
     # Time difference between telemetry points
-    telemetry["delta_t"] = (
-        telemetry
-        .groupby(["driver_number", "lap_number"])["date"]
+    groups = telemetry.groupby(["driver_number", "lap_number"])
+    telemetry["TimeSeconds"] = (
+        telemetry.groupby(["driver_number", "lap_number"])["date"]
         .diff()
         .dt.total_seconds()
+    )
+    telemetry["TimeSeconds"] = (
+        telemetry.groupby(["driver_number", "lap_number"], group_keys=False)
+        .apply(update_first_element)
     )
 
     # Convert speed to meters per second
@@ -157,7 +215,7 @@ def build_microsectors(telemetry):
 
     # Distance traveled between samples
     telemetry["delta_distance"] = (
-        telemetry["speed_mps"] * telemetry["delta_t"]
+        telemetry["speed_mps"] * telemetry["TimeSeconds"]
     )
 
     telemetry["delta_distance"] = telemetry["delta_distance"].fillna(0)
@@ -189,6 +247,16 @@ def build_microsectors(telemetry):
     return telemetry
 
 def assign_laps_to_telemetry(telemetry, laps):
+    """
+    Assign lap numbers and times to car telemetry data
+
+    Args:
+        telemetry (pd.DataFrame): Car telemetry data
+        laps (pd.DataFrame): Lap data
+
+    Returns:
+        pd.DataFrame: Car telemetry data with lap numbers
+    """
 
     driver = telemetry["driver_number"].iloc[0]
     driver_laps = laps[laps["driver_number"] == driver]
@@ -197,11 +265,13 @@ def assign_laps_to_telemetry(telemetry, laps):
     for _, lap in driver_laps.iterrows():
 
         mask = (
-            (telemetry["date"] >= lap["date_start"]) &
+            (lap["date_start"] <= telemetry["date"]) &
             (telemetry["date"] <= lap["date_end"])
         )
 
+        # assign lap number and times
         telemetry.loc[mask, "lap_number"] = lap["lap_number"]
+        telemetry.loc[mask, "LapTimeSeconds"] = lap["lap_duration"]
 
     telemetry = telemetry.dropna(subset=["lap_number"])
     telemetry["lap_number"] = telemetry["lap_number"].astype(int)
@@ -210,29 +280,26 @@ def assign_laps_to_telemetry(telemetry, laps):
 
 def download_race_data(session_key_):
     """
-    Download lap, car telemetry, weather and driver data
+    Download lap, stint, pit, weather and driver data
 
     Args:
         session_key_ (int): Race session ID
     """
 
-    laps = request_openf1_data("laps", session_key=session_key_)
-    laps["date_start"] = pd.to_datetime(laps["date_start"], format='ISO8601')
-    laps["date_end"] = (
-        laps["date_start"] + pd.to_timedelta(laps["lap_duration"], unit="s")
-    )
+    laps = request_openf1_data("laps", session_key=session_key_)    
+    stints = request_openf1_data("stints", session_key=session_key_)
+    pits = request_openf1_data("pit", session_key=session_key_)
     drivers = request_openf1_data("drivers", session_key=session_key_)
-    telemetry = download_and_process_telemetry(session_key=session_key_, drivers=drivers, laps=laps)
     weather = request_openf1_data("weather", session_key=session_key_)
 
-    return laps, telemetry, weather, drivers
+    return laps, stints, pits, weather, drivers
 
-def merge_weather(telemetry, weather):
+def merge_weather(df, weather):
     """
-    Merge Telemetry and Weather Data
+    Merge Dataframe and Weather Data on date
 
     Args:
-        telemetry (pd.DataFrame): Telemetry data
+        df (pd.DataFrame): Telemetry data
         weather (pd.DataFrame): Weather data
 
     Returns:
@@ -240,16 +307,22 @@ def merge_weather(telemetry, weather):
     """
 
     weather["date"] = pd.to_datetime(weather["date"], format='ISO8601')
-    telemetry["date"] = pd.to_datetime(telemetry["date"], format='ISO8601')
+    weather = weather[["date", "air_temperature"]] # select columns
+    
+    df["date"] = pd.to_datetime(df["date"], format='ISO8601')
 
-    telemetry = pd.merge_asof(
-        telemetry.sort_values("date"),
+    df = pd.merge_asof(
+        df.sort_values("date"),
         weather.sort_values("date"),
         on="date",
         direction="nearest"
     )
+    
+    df = df.rename(columns={
+        "air_temperature": "AirTemp"
+    })
 
-    return telemetry
+    return df
 
 def merge_drivers(df, drivers):
     """
@@ -263,6 +336,7 @@ def merge_drivers(df, drivers):
         pd.DataFrame: Merged data
     """
 
+    # select required data
     drivers = drivers[["driver_number", "name_acronym", "team_name" ]]
     df = df.merge(drivers, on="driver_number", how="left")
     df = df.rename(columns={
@@ -270,7 +344,7 @@ def merge_drivers(df, drivers):
         "team_name": "Team"
     })
 
-    return df
+    return df.drop(columns=["driver_number"])
 
 def merge_laps(df, laps):
     """
@@ -332,7 +406,7 @@ def finalize_dataset(df):
 
     return df
 
-def build_race_dataset(year, circuit_name):
+def build_datasets(year, circuit_name):
     """
     Data ingestion pipeline
 
@@ -346,32 +420,44 @@ def build_race_dataset(year, circuit_name):
 
     session_key = get_race_session(year_=year, circuit_name_=circuit_name)
 
-    laps, telemetry, weather, drivers = download_race_data(session_key_=session_key)
-
-    print("Building micro-sectors...")
-    telemetry = build_microsectors(telemetry)
-
-    print("Merging weather...")
-    df = merge_weather(telemetry, weather)
+    laps, stints, pits, weather, drivers = download_race_data(session_key_=session_key)
 
     print("Merging drivers...")
-    df = merge_drivers(df, drivers)
+    laps_df = merge_drivers(laps_df, drivers)
 
     print("Merging laps...")
-    df = merge_laps(df, laps)
+    laps_df = merge_laps(laps_df, laps)
+    
+    print("Merging drivers to lap data...")
+    laps_df = merge_drivers(laps_df, drivers)
 
     print("Finalizing dataset...")
-    df = finalize_dataset(df)
+    laps_df = finalize_dataset(laps_df)
+    
+    # ------------------------------------------------------------------------
+    # Build Micro-sector telemetry dataset
+    # ------------------------------------------------------------------------
+    print("Building Micro-sector telemetry dataset...")
+    
+    # get laps start and end data
+    telemetry = download_and_process_telemetry(
+        session_key=session_key, drivers=drivers["driver_number"], laps=laps
+    )
+    
+    print("Merging weather...")
+    telemetry_df = merge_weather(telemetry, weather)
+    
+    print("Merging drivers to micro-sector telemetry...")
+    telemetry_df = merge_drivers(telemetry_df, drivers)
 
-    return df
+    return laps_df, telemetry_df
 
 if __name__ == "__main__":
     # Grand Prix Parameters
     gp_year = 2023
     gp_circuit_name = "Yas Marina Circuit"
-    
 
-    df = build_race_dataset(
+    df = build_datasets(
         year=gp_year, circuit_name=gp_circuit_name
     )
 
